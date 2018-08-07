9c71d084e23fc44d9c9bab62967cc85e51828ed4
Niklas Gustafsson <gustafsson.niklas@gmail.com>
2018-08-07 12:45:20 +0200

    Added output variables for storage account name and connection string

--- output.tf
+++ output.tf
@@ -7,3 +7,13 @@ output "identity_tenant_id" {
   description = "The MSI identity tenant id set on the function app."
   value       = "${azurerm_function_app.functionapp.identity.0.tenant_id}"
 }
+
+output "storage_account_name" {
+  description = "The name of the storage account created for the function app"
+  value = "${azurerm_storage_account.funcsta.name}"
+}
+
+output "storage_account_connection_string" {
+  description = "Connection string to the storage account created for the function app"
+  value = "${azurerm_storage_account.funcsta.primary_connection_string}"
+}
