4fd5df4a1e1ab96886dad34e1fae7459e75ee849
Niklas Gustafsson <gustafsson.niklas@gmail.com>
2018-08-07 13:03:25 +0200

    Marked storage_account_connection_string output as sensitive

--- output.tf
+++ output.tf
@@ -16,4 +16,5 @@ output "storage_account_name" {
 output "storage_account_connection_string" {
   description = "Connection string to the storage account created for the function app"
   value = "${azurerm_storage_account.funcsta.primary_connection_string}"
+  sensitive = true
 }
